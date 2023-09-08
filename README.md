0452a3151cc1ae765a5904974d9492ded78ec1ed
Chi Vong <chivongv@gmail.com>
2023-09-08 22:33:06 +0200

    Docker compose SQL Server

---
+++ README.md
@@ -0,0 +1,8 @@
+### Example commands
+
+```
+$ mssql -u sa
+$ mssql> .databases
+$ mssql> .run db_setup.sql
+$ mssql> .tables
+```
---
+++ init_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees (Name, Salary) 
+VALUES
+    ('John Doe', 21000),
+    ('Jane Doe', 24000),
+    ('John Doe', 7000);
+GO
